0f6696aa5602b08dd1891a3b47d509b904d03eb0
vrindap24 <vrindapanchall24@gmail.com>
2025-08-04 20:48:51 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,11 @@
+#basic mathematical operation i.e add subtract multiply and divide
+n1=int(input('Enter the first number:'))
+n2=int(input('Enter the second number:'))
+Addition= n1+n2
+Subtraction=n1-n2
+Multiplication=n1*n2
+Division=n1/n2
+print('Addition: ', Addition)
+print('Subtraction: ', Subtraction)
+print('Multiplication: ', Multiplication)
+print('Divsion: ', Division)
---
+++ task2.py
@@ -0,0 +1,4 @@
+fname=input('Enter your first name: ')
+lname=input('Enter your last name: ')
+full_name= fname+' ' +lname
+print('Hello, ' + full_name + '! Welcome to the Python program.')
